a89b3301c8e1c59c8fb10548bc313ccaffcdf45e
Gabriel Silva <gasil96@gmail.com>
2019-03-19 13:55:04 -0300

    insert criado porem nao funciona

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -1,24 +1,42 @@
 package aplication;
 
 import java.sql.Connection;
-import java.util.Locale;
-import java.util.Scanner;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 import jdbc.DB;
-
 
 public class Program {
 
 	public static void main(String[] args) {
-		Locale.setDefault(Locale.US);
-		Scanner sc = new Scanner(System.in);
 		
-		//teste conexao bd
-		Connection conn = DB.getConnection();
-		DB.closeConnection();
+		Connection conn = null;
+		PreparedStatement st = null;
+
+		try {
+
+			conn = DB.getConnection();
+
+			st = conn.prepareStatement("INSERT INTO Contato" 
+					+ "(nome, cpf, idade, sexo)" 
+					+ "VALUES " 
+					+ "(?, ?, ?, ?)",
+					Statement.RETURN_GENERATED_KEYS);
+
+			st.setString(1, "Gabriel");
+			st.setString(2, "021.429.212-64");
+			st.setInt(3, 22);
+			st.setString(4, "H");
 		
-		
-		sc.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+
+			DB.closeStatement(st);
+			DB.closeConnection();
+		}
+
 	}
 
 }
--- src/jdbc/DB.java
+++ src/jdbc/DB.java
@@ -4,10 +4,14 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Properties;
 
 import javax.swing.JOptionPane;
+
+import com.sun.corba.se.spi.orbutil.fsm.Guard.Result;
 
 public class DB {
 
@@ -18,25 +22,26 @@ public class DB {
 		if (conn == null) {
 
 			try {
-				JOptionPane.showMessageDialog(null, "Conexão com o Banco Realizada");
+
 				Properties props = loadProperties();
 				String url = props.getProperty("dburl");
 				conn = DriverManager.getConnection(url, props);
-				
+				JOptionPane.showMessageDialog(null, "Conexão com o Banco Realizada");
+
 			} catch (SQLException e) {
-				
+
 				JOptionPane.showMessageDialog(null, "Erro ao conectar no banco");
 				throw new DbException(e.getMessage());
-				
+
 			}
 		}
-		
+
 		return conn;
 	}
 
 	// metodo para fechar a conexao
 	public static void closeConnection() {
-		
+
 		if (conn != null) {
 			try {
 				conn.close();
@@ -50,17 +55,39 @@ public class DB {
 
 	// metodo para carregar propriedas do db.properties
 	private static Properties loadProperties() {
-		
+
 		try (FileInputStream fs = new FileInputStream("db.properties")) {
-			
+
 			Properties props = new Properties();
 			props.load(fs);
 			return props;
-			
+
 		} catch (IOException e) {
-			
+
 			throw new DbException(e.getMessage());
-			
+
+		}
+	}
+
+	public static void closeStatement(Statement st) {
+		if (st != null) {
+			try {
+				st.close();
+
+			} catch (SQLException e) {
+				throw new DbException(e.getMessage());
+			}
+		}
+	}
+
+	public static void closeResultSet(ResultSet rs) {
+		if (rs != null) {
+			try {
+				rs.close();
+
+			} catch (SQLException e) {
+				throw new DbException(e.getMessage());
+			}
 		}
 	}
 }
