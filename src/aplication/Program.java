d57065532ae6490163cc94ac8bb51afe71e72dee
Gabriel Silva <gasil96@gmail.com>
2019-03-21 10:52:17 -0300

    conexao e inserção realizada

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -1,7 +1,8 @@
 package aplication;
 
-
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 import jdbc_connection.DB;
 
@@ -9,9 +10,34 @@ public class Program {
 
 	public static void main(String[] args) {
 
-		Connection conn = DB.getConnection();
-		DB.closeConnection();
 		
+		Connection conn = null;
+		PreparedStatement st = null;
+		
+		try {
+			conn = DB.getConnection();
+			st = conn.prepareStatement(
+					"INSERT INTO agenda.contato " // remover apostrofe
+					+ "(nome, cpf, idade, sexo) " //remove apostrofe
+					+ "VALUES "
+					+ "(?, ?, ?, ?)"); //pont  virgula e apost sql apagado
+			
+			st.setString(1, "Gerusa");
+			st.setString(2, "021.429.212-64");
+			st.setInt(3, 39);
+			st.setString(4, "Feminino");
+
+			int rowsAffected = st.executeUpdate();
+			System.out.println("Done! Rows Affected " + rowsAffected);
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		finally {
+			DB.closeStatement(st);
+			DB.closeConnection();
+		}
 	}
 
 }
