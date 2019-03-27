0b1ab59b35a1bab3873c79d503b212a43160f1c3
Gabriel Silva <gasil96@gmail.com>
2019-03-27 10:10:09 -0300

    Commit de Backup

--- src/crud/aplication/ScreenMain.java
+++ src/crud/aplication/ScreenMain.java
@@ -1,25 +1,21 @@
 package crud.aplication;
 
-import java.awt.BorderLayout;
+import java.awt.Button;
 import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.Label;
+import java.awt.TextField;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.ContainerAdapter;
-import java.awt.event.ContainerEvent;
-import java.awt.event.InputMethodEvent;
-import java.awt.event.InputMethodListener;
 
-import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JLayeredPane;
-import javax.swing.border.EmptyBorder;
+import javax.swing.JOptionPane;
 
 import crud.dao.ContatoDAO;
 import crud.entities.Contato;
+import java.awt.Color;
 
 public class ScreenMain extends JFrame {
-
-	private JLayeredPane contentPane;
 
 	/**
 	 * Launch the application.
@@ -43,51 +39,89 @@ public class ScreenMain extends JFrame {
 	 */
 	public ScreenMain() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 478, 411);
-		contentPane = new JLayeredPane();
-		contentPane.addInputMethodListener(new InputMethodListener() {
-			public void caretPositionChanged(InputMethodEvent event) {
-			}
+		setBounds(100, 100, 439, 415);
+		getContentPane().setLayout(null);
+		
+		
+		Label label = new Label("NOME:");
+		label.setFont(new Font("Dialog", Font.PLAIN, 16));
+		label.setBounds(10, 89, 49, 22);
+		getContentPane().add(label);
+		
+		
+		Label label_1 = new Label("IDADE:");
+		label_1.setFont(new Font("Dialog", Font.PLAIN, 16));
+		label_1.setBounds(10, 168, 55, 22);
+		getContentPane().add(label_1);
+		
+		Label label_2 = new Label("SEXO:");
+		label_2.setFont(new Font("Dialog", Font.PLAIN, 16));
+		label_2.setBounds(10, 207, 49, 22);
+		getContentPane().add(label_2);
+		
+		Label label_3 = new Label("Masculino/Feminino/Não Declarado");
+		label_3.setForeground(new Color(255, 0, 0));
+		label_3.setBounds(204, 207, 171, 22);
+		getContentPane().add(label_3);
+		
+		Label label_4 = new Label("CPF:");
+		label_4.setFont(new Font("Dialog", Font.PLAIN, 16));
+		label_4.setBounds(10, 127, 49, 22);
+		getContentPane().add(label_4);
 
-			public void inputMethodTextChanged(InputMethodEvent event) {
-
-			}
-		});
-		contentPane.addContainerListener(new ContainerAdapter() {
-			@Override
-			public void componentAdded(ContainerEvent e) {
-			}
-		});
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		setContentPane(contentPane);
-		contentPane.setLayout(new BorderLayout(0, 0));
-
-		JButton btnContatoAdd = new JButton("Adicionar Contatos no SOUT**TESTE***");
-		btnContatoAdd.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-
-				ContatoDAO cttDAO = new ContatoDAO();
-				Contato contato = new Contato();
-				contato.setNome("Gabriel Silva");
-				contato.setCpf("021.429.212.64");
-				contato.setIdade(23);
-				contato.setSexo("Masculino");
-				cttDAO.create(contato);
-
-			}
-		});
-		contentPane.add(btnContatoAdd, BorderLayout.SOUTH);
-
-		JButton btnRemoverContatosteste = new JButton("Remover Contatos ***TESTE***");
-		btnRemoverContatosteste.addActionListener(new ActionListener() {
+		
+		TextField txtNome = new TextField();
+		txtNome.setBounds(71, 89, 293, 22);
+		getContentPane().add(txtNome);
+		
+		TextField txtCpf = new TextField();
+		txtCpf.setBounds(71, 127, 184, 22);
+		getContentPane().add(txtCpf);
+		
+		TextField txtIdade = new TextField();
+		txtIdade.setBounds(71, 168, 33, 22);
+		getContentPane().add(txtIdade);
+		
+		TextField txtSexo = new TextField();
+		txtSexo.setBounds(71, 207, 127, 22);
+		getContentPane().add(txtSexo);
+		
+		
+		Button button = new Button("Salvar");
+		button.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				
+				String nome = txtNome.getText();
+				String cpf = txtCpf.getText();
+				int idade = Integer.parseInt(txtIdade.getText());
+				String sexo = txtSexo.getText();
 				ContatoDAO cttDAO = new ContatoDAO();
-				cttDAO.remove(0);
-							
+				Contato contato = new Contato();
+				contato.setNome(nome); 
+				contato.setCpf(cpf);
+				contato.setIdade(idade);
+				contato.setSexo(sexo);
+				cttDAO.create(contato);
+				try {
+					cttDAO.create(contato);
+					JOptionPane.showMessageDialog(null, "Novo Contato Cadastrado!");
+				} catch (Exception e2) {
+					JOptionPane.showMessageDialog(null, "Problemas ao Cadastrar Contato!");
+				}
 			}
 		});
-		contentPane.add(btnRemoverContatosteste, BorderLayout.NORTH);
+		button.setBounds(321, 345, 70, 22);
+		getContentPane().add(button);
+		
+		Label label_5 = new Label("2 Digitos");
+		label_5.setForeground(Color.RED);
+		label_5.setBounds(110, 168, 171, 22);
+		getContentPane().add(label_5);
+		
+		Label label_6 = new Label("Com pontos ' . ' e Traço ' - '");
+		label_6.setForeground(Color.RED);
+		label_6.setBounds(261, 127, 171, 22);
+		getContentPane().add(label_6);
 	}
-
 }
+
