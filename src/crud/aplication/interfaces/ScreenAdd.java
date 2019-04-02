ebcc8828394a690ecbc87460f1cea83d2408d2e7
Gabriel Silva <gasil96@gmail.com>
2019-04-02 13:06:42 -0300

    tabela add modificada

--- src/crud/aplication/interfaces/ScreenAdd.java
+++ src/crud/aplication/interfaces/ScreenAdd.java
@@ -21,6 +21,7 @@ import crud.entities.Contato;
 import crud.jdbc.connection.DB;
 
 import javax.swing.JButton;
+import javax.swing.JList;
 
 public class ScreenAdd extends JFrame {
 
@@ -40,59 +41,44 @@ public class ScreenAdd extends JFrame {
 
 	public ScreenAdd() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 561, 524);
+		setBounds(100, 100, 468, 524);
 		getContentPane().setLayout(null);
 
 		Label label = new Label("NOME:");
 		label.setFont(new Font("Dialog", Font.PLAIN, 16));
-		label.setBounds(10, 89, 49, 22);
+		label.setBounds(63, 89, 55, 22);
 		getContentPane().add(label);
 
 		Label label_1 = new Label("IDADE:");
 		label_1.setFont(new Font("Dialog", Font.PLAIN, 16));
-		label_1.setBounds(10, 168, 55, 22);
+		label_1.setBounds(63, 168, 55, 22);
 		getContentPane().add(label_1);
 
 		Label label_2 = new Label("SEXO:");
 		label_2.setFont(new Font("Dialog", Font.PLAIN, 16));
-		label_2.setBounds(10, 207, 49, 22);
+		label_2.setBounds(69, 207, 49, 22);
 		getContentPane().add(label_2);
-
-		Label label_3 = new Label("Masculino/Feminino/N�o Declarado");
-		label_3.setForeground(new Color(255, 0, 0));
-		label_3.setBounds(204, 207, 193, 22);
-		getContentPane().add(label_3);
 
 		Label label_4 = new Label("CPF:");
 		label_4.setFont(new Font("Dialog", Font.PLAIN, 16));
-		label_4.setBounds(10, 127, 49, 22);
+		label_4.setBounds(80, 127, 44, 22);
 		getContentPane().add(label_4);
 
 		TextField txtNome = new TextField("");
-		txtNome.setBounds(71, 89, 293, 22);
+		txtNome.setBounds(124, 89, 293, 22);
 		getContentPane().add(txtNome);
 
 		TextField txtCpf = new TextField();
-		txtCpf.setBounds(71, 127, 184, 22);
+		txtCpf.setBounds(124, 127, 184, 22);
 		getContentPane().add(txtCpf);
 
 		TextField txtIdade = new TextField();
-		txtIdade.setBounds(71, 168, 33, 22);
+		txtIdade.setBounds(124, 168, 33, 22);
 		getContentPane().add(txtIdade);
 
 		TextField txtSexo = new TextField();
-		txtSexo.setBounds(71, 207, 127, 22);
+		txtSexo.setBounds(124, 207, 127, 22);
 		getContentPane().add(txtSexo);
-
-		Label label_5 = new Label("2 Digitos");
-		label_5.setForeground(Color.RED);
-		label_5.setBounds(110, 168, 171, 22);
-		getContentPane().add(label_5);
-
-		Label label_6 = new Label("Com pontos ' . ' e Tra�o ' - '");
-		label_6.setForeground(Color.RED);
-		label_6.setBounds(261, 127, 171, 22);
-		getContentPane().add(label_6);
 
 		JButton btnNewButton = new JButton("Voltar");
 		btnNewButton.addActionListener(new ActionListener() {
@@ -131,6 +117,42 @@ public class ScreenAdd extends JFrame {
 		});
 		btnNewButton_1.setBounds(344, 452, 89, 23);
 		getContentPane().add(btnNewButton_1);
+		
+		Label label_7 = new Label("EMAIL:");
+		label_7.setFont(new Font("Dialog", Font.PLAIN, 16));
+		label_7.setBounds(68, 250, 55, 22);
+		getContentPane().add(label_7);
+		
+		TextField txtEmail = new TextField();
+		txtEmail.setBounds(124, 250, 184, 22);
+		getContentPane().add(txtEmail);
+		
+		Label label_8 = new Label("CELULAR:");
+		label_8.setFont(new Font("Dialog", Font.PLAIN, 16));
+		label_8.setBounds(39, 292, 84, 22);
+		getContentPane().add(label_8);
+		
+		TextField textField = new TextField();
+		textField.setBounds(124, 292, 184, 22);
+		getContentPane().add(textField);
+		
+		Label label_9 = new Label("COMERCIAL: ");
+		label_9.setFont(new Font("Dialog", Font.PLAIN, 16));
+		label_9.setBounds(21, 329, 102, 22);
+		getContentPane().add(label_9);
+		
+		TextField textField_1 = new TextField();
+		textField_1.setBounds(124, 329, 184, 22);
+		getContentPane().add(textField_1);
+		
+		Label label_10 = new Label("RESIDENCIAL:");
+		label_10.setFont(new Font("Dialog", Font.PLAIN, 16));
+		label_10.setBounds(10, 364, 113, 22);
+		getContentPane().add(label_10);
+		
+		TextField textField_2 = new TextField();
+		textField_2.setBounds(124, 364, 184, 22);
+		getContentPane().add(textField_2);
 	}
 
 	private static void addPopup(Component component, final JPopupMenu popup) {
