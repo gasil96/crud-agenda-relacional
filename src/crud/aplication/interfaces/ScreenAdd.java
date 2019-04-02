fafece77e9434687a522c344e09bb2933d74f9df
Gabriel Silva <gasil96@gmail.com>
2019-04-02 13:19:12 -0300

    tela add e update com comunica inserido

--- src/crud/aplication/interfaces/ScreenAdd.java
+++ src/crud/aplication/interfaces/ScreenAdd.java
@@ -13,6 +13,7 @@ import java.sql.Connection;
 
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
+import javax.swing.JPanel;
 import javax.swing.JPopupMenu;
 import javax.swing.JToolBar;
 
@@ -40,44 +41,51 @@ public class ScreenAdd extends JFrame {
 	}
 
 	public ScreenAdd() {
+		
+
+		JPanel panel = new JPanel();
+		panel.setBounds(0, 0, 452, 85);
+		getContentPane().add(panel);
+
+		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 468, 524);
 		getContentPane().setLayout(null);
 
 		Label label = new Label("NOME:");
 		label.setFont(new Font("Dialog", Font.PLAIN, 16));
-		label.setBounds(63, 89, 55, 22);
+		label.setBounds(62, 91, 60, 22);
 		getContentPane().add(label);
 
 		Label label_1 = new Label("IDADE:");
 		label_1.setFont(new Font("Dialog", Font.PLAIN, 16));
-		label_1.setBounds(63, 168, 55, 22);
+		label_1.setBounds(68, 147, 55, 22);
 		getContentPane().add(label_1);
 
 		Label label_2 = new Label("SEXO:");
 		label_2.setFont(new Font("Dialog", Font.PLAIN, 16));
-		label_2.setBounds(69, 207, 49, 22);
+		label_2.setBounds(72, 175, 52, 22);
 		getContentPane().add(label_2);
 
 		Label label_4 = new Label("CPF:");
 		label_4.setFont(new Font("Dialog", Font.PLAIN, 16));
-		label_4.setBounds(80, 127, 44, 22);
+		label_4.setBounds(80, 119, 44, 22);
 		getContentPane().add(label_4);
 
 		TextField txtNome = new TextField("");
-		txtNome.setBounds(124, 89, 293, 22);
+		txtNome.setBounds(123, 91, 293, 22);
 		getContentPane().add(txtNome);
 
 		TextField txtCpf = new TextField();
-		txtCpf.setBounds(124, 127, 184, 22);
+		txtCpf.setBounds(124, 119, 184, 22);
 		getContentPane().add(txtCpf);
 
 		TextField txtIdade = new TextField();
-		txtIdade.setBounds(124, 168, 33, 22);
+		txtIdade.setBounds(126, 147, 36, 22);
 		getContentPane().add(txtIdade);
 
 		TextField txtSexo = new TextField();
-		txtSexo.setBounds(124, 207, 127, 22);
+		txtSexo.setBounds(125, 175, 124, 22);
 		getContentPane().add(txtSexo);
 
 		JButton btnNewButton = new JButton("Voltar");
@@ -89,7 +97,7 @@ public class ScreenAdd extends JFrame {
 				dispose();
 			}
 		});
-		btnNewButton.setBounds(253, 452, 89, 23);
+		btnNewButton.setBounds(262, 452, 89, 23);
 		getContentPane().add(btnNewButton);
 
 		JButton btnNewButton_1 = new JButton("Salvar");
@@ -115,43 +123,43 @@ public class ScreenAdd extends JFrame {
 			}
 
 		});
-		btnNewButton_1.setBounds(344, 452, 89, 23);
+		btnNewButton_1.setBounds(353, 452, 89, 23);
 		getContentPane().add(btnNewButton_1);
 		
 		Label label_7 = new Label("EMAIL:");
 		label_7.setFont(new Font("Dialog", Font.PLAIN, 16));
-		label_7.setBounds(68, 250, 55, 22);
+		label_7.setBounds(68, 203, 55, 22);
 		getContentPane().add(label_7);
 		
 		TextField txtEmail = new TextField();
-		txtEmail.setBounds(124, 250, 184, 22);
+		txtEmail.setBounds(124, 203, 184, 22);
 		getContentPane().add(txtEmail);
 		
 		Label label_8 = new Label("CELULAR:");
 		label_8.setFont(new Font("Dialog", Font.PLAIN, 16));
-		label_8.setBounds(39, 292, 84, 22);
+		label_8.setBounds(39, 231, 84, 22);
 		getContentPane().add(label_8);
 		
 		TextField textField = new TextField();
-		textField.setBounds(124, 292, 184, 22);
+		textField.setBounds(124, 231, 184, 22);
 		getContentPane().add(textField);
 		
 		Label label_9 = new Label("COMERCIAL: ");
 		label_9.setFont(new Font("Dialog", Font.PLAIN, 16));
-		label_9.setBounds(21, 329, 102, 22);
+		label_9.setBounds(21, 259, 102, 22);
 		getContentPane().add(label_9);
 		
 		TextField textField_1 = new TextField();
-		textField_1.setBounds(124, 329, 184, 22);
+		textField_1.setBounds(124, 259, 184, 22);
 		getContentPane().add(textField_1);
 		
 		Label label_10 = new Label("RESIDENCIAL:");
 		label_10.setFont(new Font("Dialog", Font.PLAIN, 16));
-		label_10.setBounds(10, 364, 113, 22);
+		label_10.setBounds(10, 287, 113, 22);
 		getContentPane().add(label_10);
 		
 		TextField textField_2 = new TextField();
-		textField_2.setBounds(124, 364, 184, 22);
+		textField_2.setBounds(124, 287, 184, 22);
 		getContentPane().add(textField_2);
 	}
 
--- src/crud/aplication/interfaces/ScreenUpdate.java
+++ src/crud/aplication/interfaces/ScreenUpdate.java
@@ -16,6 +16,7 @@ import javax.swing.JPopupMenu;
 
 import crud.dao.ContatoDAO;
 import crud.entities.Contato;
+import javax.swing.JPanel;
 
 public class ScreenUpdate extends JFrame {
 
@@ -58,47 +59,42 @@ public class ScreenUpdate extends JFrame {
 			e1.printStackTrace();
 		}
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 561, 524);
+		setBounds(100, 100, 468, 524);
 		getContentPane().setLayout(null);
 
 		Label label = new Label("NOME:");
 		label.setFont(new Font("Dialog", Font.PLAIN, 16));
-		label.setBounds(10, 89, 49, 22);
+		label.setBounds(69, 119, 55, 22);
 		getContentPane().add(label);
 
 		Label label_1 = new Label("IDADE:");
 		label_1.setFont(new Font("Dialog", Font.PLAIN, 16));
-		label_1.setBounds(10, 168, 55, 22);
+		label_1.setBounds(68, 175, 55, 22);
 		getContentPane().add(label_1);
 
 		Label label_2 = new Label("SEXO:");
 		label_2.setFont(new Font("Dialog", Font.PLAIN, 16));
-		label_2.setBounds(10, 207, 49, 22);
+		label_2.setBounds(74, 203, 49, 22);
 		getContentPane().add(label_2);
-
-		Label label_3 = new Label("Masculino/Feminino/N�o Declarado");
-		label_3.setForeground(new Color(255, 0, 0));
-		label_3.setBounds(204, 207, 193, 22);
-		getContentPane().add(label_3);
 
 		Label label_4 = new Label("CPF:");
 		label_4.setFont(new Font("Dialog", Font.PLAIN, 16));
-		label_4.setBounds(10, 127, 49, 22);
+		label_4.setBounds(85, 147, 39, 22);
 		getContentPane().add(label_4);
 
 		// gambi
 		String idSTG = Integer.toString(contato.getId_contato());
 
 		TextField txtId = new TextField(idSTG);
-		txtId.setBounds(71, 61, 33, 22);
+		txtId.setBounds(130, 91, 33, 22);
 		getContentPane().add(txtId);
 
 		TextField txtNome = new TextField(contato.getNome());
-		txtNome.setBounds(71, 89, 293, 22);
+		txtNome.setBounds(130, 119, 293, 22);
 		getContentPane().add(txtNome);
 
 		TextField txtCpf = new TextField(contato.getCpf());
-		txtCpf.setBounds(71, 127, 184, 22);
+		txtCpf.setBounds(130, 147, 184, 22);
 		getContentPane().add(txtCpf);
 		
 		//
@@ -106,26 +102,16 @@ public class ScreenUpdate extends JFrame {
 		String idadeSTG = Integer.toString(contato.getIdade());
 
 		TextField txtIdade = new TextField(idadeSTG);
-		txtIdade.setBounds(71, 168, 33, 22);
+		txtIdade.setBounds(129, 175, 33, 22);
 		getContentPane().add(txtIdade);
 
 		TextField txtSexo = new TextField(contato.getSexo());
-		txtSexo.setBounds(71, 207, 127, 22);
+		txtSexo.setBounds(129, 203, 127, 22);
 		getContentPane().add(txtSexo);
-
-		Label label_5 = new Label("2 Digitos");
-		label_5.setForeground(Color.RED);
-		label_5.setBounds(110, 168, 171, 22);
-		getContentPane().add(label_5);
-
-		Label label_6 = new Label("Com pontos ' . ' e Tra�o ' - '");
-		label_6.setForeground(Color.RED);
-		label_6.setBounds(261, 127, 171, 22);
-		getContentPane().add(label_6);
 
 		Label label_7 = new Label("ID:");
 		label_7.setFont(new Font("Dialog", Font.PLAIN, 16));
-		label_7.setBounds(10, 61, 55, 22);
+		label_7.setBounds(101, 91, 23, 22);
 		getContentPane().add(label_7);
 
 		JButton btnNewButton_1 = new JButton("Alterar");
@@ -166,8 +152,48 @@ public class ScreenUpdate extends JFrame {
 			}
 
 		});
-		btnNewButton_1.setBounds(344, 452, 89, 23);
+		btnNewButton_1.setBounds(353, 452, 89, 23);
 		getContentPane().add(btnNewButton_1);
+		
+		TextField textField = new TextField();
+		textField.setBounds(129, 231, 184, 22);
+		getContentPane().add(textField);
+		
+		Label label_8 = new Label("EMAIL:");
+		label_8.setFont(new Font("Dialog", Font.PLAIN, 16));
+		label_8.setBounds(68, 231, 55, 22);
+		getContentPane().add(label_8);
+		
+		Label label_9 = new Label("CELULAR:");
+		label_9.setFont(new Font("Dialog", Font.PLAIN, 16));
+		label_9.setBounds(44, 259, 79, 22);
+		getContentPane().add(label_9);
+		
+		TextField textField_1 = new TextField();
+		textField_1.setBounds(129, 259, 184, 22);
+		getContentPane().add(textField_1);
+		
+		Label label_10 = new Label("COMERCIAL: ");
+		label_10.setFont(new Font("Dialog", Font.PLAIN, 16));
+		label_10.setBounds(21, 287, 102, 22);
+		getContentPane().add(label_10);
+		
+		TextField textField_2 = new TextField();
+		textField_2.setBounds(129, 287, 184, 22);
+		getContentPane().add(textField_2);
+		
+		Label label_11 = new Label("RESIDENCIAL:");
+		label_11.setFont(new Font("Dialog", Font.PLAIN, 16));
+		label_11.setBounds(10, 315, 113, 22);
+		getContentPane().add(label_11);
+		
+		TextField textField_3 = new TextField();
+		textField_3.setBounds(129, 315, 184, 22);
+		getContentPane().add(textField_3);
+		
+		JPanel panel = new JPanel();
+		panel.setBounds(0, 0, 452, 85);
+		getContentPane().add(panel);
 
 		
 	}
