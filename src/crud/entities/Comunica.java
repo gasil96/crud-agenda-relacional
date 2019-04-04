5bd60ff22944556beea54a1d0e38e1ea0b86f607
Gabriel Silva <gasil96@gmail.com>
2019-04-04 12:18:24 -0300

    enum cont

--- src/crud/entities/Comunica.java
+++ src/crud/entities/Comunica.java
@@ -2,9 +2,13 @@ package crud.entities;
 
 public class Comunica {
 
+	public enum tipo {EMAIL, CELULAR, TELEFONE_RESIDENCIAL, TELEFONE_COMERCIAL};
+
 	private int id_comunica;
+	private Contato id_contato;
+	private tipo Tipo;
+	private String registro;
 	
-	private int id_contato;
 	
 	public Comunica() {
 		
