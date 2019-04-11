e09769408778fa80cafb9816004249dd866a2985
Gabriel Silva <gasil96@gmail.com>
2019-04-11 09:38:14 -0300

    atualizando

--- src/crud/jdbc/connection/DbException.java
+++ src/crud/jdbc/connection/DbException.java
@@ -5,7 +5,8 @@ public class DbException extends RuntimeException {
 	private static final long serialVersionUID = 1L;
 
 	public DbException(String msg) {
-		
+		// TODO - alguma coisa foi feita
+		// TODO - alguma coisa foi feita denovo
 		super(msg);
 		
 	}
